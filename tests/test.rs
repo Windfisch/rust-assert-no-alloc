2396c3372ed89fa52a73e9825af59d9d3039fb7a
Florian Jung <flo@windfis.ch>
2020-06-24 01:55:11 +0200

    test catch_unwind behaviour

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,5 @@
 use assert_no_alloc::*;
+use std::panic::catch_unwind;
 
 #[global_allocator]
 static A: AllocDisabler = AllocDisabler;
@@ -73,32 +74,59 @@ fn forbidden_after_nested() {
 	assert_eq!(check_and_reset(), true);
 }
 
-/*
 #[test]
-fn ok_after_unwind() {
-	let result = catch_unwind(|| {
-		assert_no_alloc(|| {
-			do_alloc();
+fn unwind_ok() {
+	assert_eq!(check_and_reset(), false);
+	assert_no_alloc(|| {
+		let r = catch_unwind(|| {
+			assert_no_alloc(|| {
+				panic!();
+			});
 		});
+		assert!(r.is_err());
 	});
-
-	assert!(result.is_err());
-
+	reset_violation_count(); // unwinding might have allocated memory; we don't care about that.
 	do_alloc();
+	assert_eq!(check_and_reset(), false);
 }
 
 #[test]
-#[should_panic(expected = "Tried to (de)allocate memory in a thread forbids allocator calls!")]
-fn forbidden_after_unwind() {
-	let result = catch_unwind(|| {
+fn unwind_nested() {
+	assert_eq!(check_and_reset(), false);
+	assert_no_alloc(|| {
+		let r = catch_unwind(|| {
+			assert_no_alloc(|| {
+				panic!();
+			});
+		});
+		assert!(r.is_err());
+		
+		reset_violation_count(); // unwinding might have allocated memory; we don't care about that.
+		do_alloc();
+		assert_eq!(check_and_reset(), true);
+	});
+}
+
+#[test]
+fn unwind_nested2() {
+	assert_eq!(check_and_reset(), false);
+	assert_no_alloc(|| {
 		assert_no_alloc(|| {
-			do_alloc();
+		let r = catch_unwind(|| {
+			assert_no_alloc(|| {
+				assert_no_alloc(|| {
+					panic!();
+				});
+			});
+		});
+		assert!(r.is_err());
+		
+		reset_violation_count(); // unwinding might have allocated memory; we don't care about that.
+		do_alloc();
+		assert_eq!(check_and_reset(), true);
 		});
 	});
-
-	assert!(result.is_err());
-
-	assert_no_alloc(|| {
-		do_alloc();
-	});
-}*/
+	reset_violation_count(); // unwinding might have allocated memory; we don't care about that.
+	do_alloc();
+	assert_eq!(check_and_reset(), false);
+}
